fe59b3f8e5a4efec0adb1301faa5d0e00f1dc5c8
hrithik20007 <darthvader20137@gmail.com>
2021-07-02 23:59:38 +0530

    VectorsAndPair2

--- ApniKaksha/VectorsAndPairs/pair.cpp
+++ ApniKaksha/VectorsAndPairs/pair.cpp
@@ -8,11 +8,20 @@ This program is an intro to pairs in C++.
 using namespace std;
 
 int main(){
-	pair<int,char> p;
-	p.first=3;
-	p.second='a';
+	pair<int,char> p1;
+	pair<int,char> p2;
+	
+	p1.first=3;					//Value of 1st element
+	p1.second='a';				//Value of 2nd element
 
-	cout<<p.first<<endl;		//Prints first element 
-	cout<<p.second<<endl;		//Prints second element
+	p2=make_pair(9,'a');		//make_pair() returns a pair whose value we provide at the time of writing the function. We can do this instead of .first etc.
+
+	cout<<p1.first<<endl;		//Prints first element 
+	cout<<p1.second<<endl;		//Prints second element
+
+	cout<<endl;
+
+	cout<<p2.first<<endl;		//Prints first element 
+	cout<<p2.second<<endl;		//Prints second element
 	return 0;
 }
